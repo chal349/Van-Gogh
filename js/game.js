54b8105437b7e4fd95b06354e95977aee48f5611
Corey Hall <79333726+chal349@users.noreply.github.com>
2022-07-01 11:34:31 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -63,7 +63,7 @@ getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
         //go to the end page
-        return window.location.assign('/Art-Quiz/end.html');
+        return window.location.assign("/");
     }
 
     questionCounter++;
